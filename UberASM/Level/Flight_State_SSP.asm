af1fe6d295adee61b4eaa0c33fd4030215fe04a0
binavik <binavik@hotmail.com>
2021-10-27 18:22:24 -0700

    Option to always give flight out of a pipe

--- UberASM/Level/Flight_State_SSP.asm
+++ UberASM/Level/Flight_State_SSP.asm
@@ -1,7 +1,15 @@
 ;from SSPDef\Defines.asm, must match !Freeram_SSP_EntrExtFlg
 !inPipeFlag = $62		
 
-;2 bytes
+;0 = no, 1 = yes
+;this option conserves the takeoff_meter value when you enter the pipe
+;if 0 it'll load whatever is in !takeoff_meter 
+!conserve_takeoff_meter = 1
+
+;only used if above is 0
+!takeoff_meter = #$60
+
+;2 bytes if !conserve_takeoff_meter is 1, 1 byte otherwise
 ;1st byte, flag for if we entered a pipe and should store the takeoff meter
 ;2nd byte, value of takeoff meter when first entering the pipe and what to restore when exiting the pipe
 ;This could theoretically be 1 byte but issues could arise when entering a pipe with 0 takeoff meter
@@ -10,7 +18,6 @@
 
 init:
 	STZ !freeRAM
-	STZ !freeRAM+1
 	RTL
 	
 main:
@@ -18,7 +25,11 @@ main:
 	BEQ .checkInPipe
 	LDA !inPipeFlag
 	BNE .return
+if !conserve_takeoff_meter
 	LDA !freeRAM+1
+else
+	LDA !takeoff_meter
+endif
 	STA $149F|!addr
 	STZ !freeRAM
 	RTL
@@ -28,7 +39,9 @@ main:
 	CMP #$02
 	BEQ .return
 	INC !freeRAM
+if !conserve_takeoff_meter
 	LDA $149F|!addr
 	STA !freeRAM+1	
+endif
 .return
 	RTL
