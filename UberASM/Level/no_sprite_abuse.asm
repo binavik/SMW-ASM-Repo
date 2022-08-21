1d3f00af9d45cbd94a290929c5e37e131afd5dd2
binavik <binavik@hotmail.com>
2022-08-21 14:14:01 -0700

    Updating to add check for star killing sprites

--- UberASM/Level/no_sprite_abuse.asm
+++ UberASM/Level/no_sprite_abuse.asm
@@ -1,7 +1,17 @@
 ;checks if a sprite is dying, and if so also kills mario
 
+init:
+	STZ $18D2|!addr
+	RTL
+
+kill:
+	JSL $00F5B7|!bank
+	RTL
+
 main:
 	LDX #!sprite_slots
+	LDA $18D2|!addr
+	BNE kill
 .loop
 	LDA !14C8,x
 	BEQ .next
@@ -9,7 +19,7 @@ main:
 	BEQ .next
 	CMP #$06
 	BPL .next
-	JSL $00F5B7|!bank
+	BRA kill
 .next
 	DEX
 	BPL .loop
