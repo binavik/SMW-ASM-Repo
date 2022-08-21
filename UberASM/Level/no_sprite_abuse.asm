83e01422702b53316ae5cbb9f65960a052ab8cc5
binavik <binavik@hotmail.com>
2022-08-21 14:33:05 -0700

    Update to give option to either kill or hurt mario

--- UberASM/Level/no_sprite_abuse.asm
+++ UberASM/Level/no_sprite_abuse.asm
@@ -1,15 +1,23 @@
 ;checks if a sprite is dying, and if so also kills mario
 
+;0 for hurt, 1 for kill
+!kill = 1
+
 init:
 	STZ $18D2|!addr
 	RTL
 
 kill:
+if !kill == 0
 	JSL $00F5B7|!bank
+else
+	JSL $00F606|!bank
+endif
+	
 	RTL
 
 main:
-	LDX #!sprite_slots
+	LDX #!sprite_slots-1
 	LDA $18D2|!addr
 	BNE kill
 .loop
